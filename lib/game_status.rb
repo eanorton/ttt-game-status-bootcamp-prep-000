5d77459f0d31832a30068346b0a97418e9b3d1fc
Edward Norton <edwardnorton@icloud.com>
2018-01-12 23:31:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,4 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   if WIN_COMBINATIONS.each do |win|
-    WIN_COMBINATIONS[0,1,2,3,4,5,6,7].all?
-  else FALSE
-end
+    WIN_COMBINATIONS[0,1,2,3,4,5,6,7].all? 
